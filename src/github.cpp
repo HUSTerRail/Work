2fea5cf17d7cd21e0ce309baf9db153acf46b670
xuzq <1176454374@qq.com>
2023-03-23 23:12:47 +0800

    百度题3

--- src/github.cpp
+++ src/github.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World777!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World888!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
