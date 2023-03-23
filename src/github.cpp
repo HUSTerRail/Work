a40b35dea996677716981b0d7ba333c960aa0b7a
linyu <1611890852@qq.com>
2023-03-23 23:09:34 +0800

    百度题1

--- src/github.cpp
+++ src/github.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World555!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World666!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
