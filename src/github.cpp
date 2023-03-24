69c9f29f567bc7e68b8a9f165a7c0a378578c811
xuzq <1176454374@qq.com>
2023-03-24 11:21:47 +0800

    mihayou1

--- src/github.cpp
+++ src/github.cpp
@@ -1,46 +1,26 @@
 //============================================================================
-// Name        : League_building.cpp
+// Name        : MiHaYou1.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#include <vector>
-#include <map>
-
-//目前唯一想到的方法是采用递归的方法2
-
-class Solution{
-public:
-	void find(int n, int t, vector<string>& nums, map<char, int>& map_count, map<char, int>& map_price){
-		for(int i = 0; i < nums[t].size(); i++){
-
-		}
-	}
-};
 
 int main() {
-	Solution solve;
-	int n;
-	int num;
 	string s;
-	cin >> n;
-	vector<string> nums;
-	for(int i = 0; i < n; i++){
-		cin >> s;
-		nums.push_back(s);
+	cin >> s;
+	for(int i = 1; i < s.size(); i++){
+		if(s[i] < s[i-1])
+		{
+			char temp = s[i];
+			s[i] = s[i-1];
+			s[i-1] = temp;
+			break;
+		}
 	}
-	map<char, int> map_count;
-	map<char, int> map_price;
-	cin >> map_count['A'];
-	cin >> map_price['A'];
-	cin >> map_count['B'];
-	cin >> map_price['B'];
-	cin >> map_count['C'];
-	cin >> map_price['C'];
-	solve.find(n, 0, nums, map_count, map_price);
+	cout << s;
 	return 0;
 }
