5899c7705a64daff3e6a6b6312f4e30494f774f4
xuzq <1176454374@qq.com>
2023-04-26 15:51:57 +0800

    1

--- src/work.cpp
+++ src/work.cpp
@@ -12,44 +12,6 @@ class Solution {
 public:
 	int func(string s, int longth){
 		int result = 0;
-		int flag = 1;
-		int flag2 = 0;
-		for(int i = 0; i < longth; i++){
-			if( flag == 1 && s[i] == '1'){
-				flag = 0;
-				result += 1;
-			}else{
-				flag = 1;
-
-			}
-		}
-		return result;
-	}
-
-};
-
-int main() {
-	Solution solve;
-	int n,longth;
-	string str;
-	cin >> n;
-
-	int result = 0;
-	for(int i = 0; i<n; i++){
-		cin >> longth >> str;
-		result = solve.func(str, longth);
-		cout<<result<<endl;
-	}
-
-	return 0;
-};
-
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-	int func(string s, int longth){
-		int result = 0;
 		int flag = 0;
 		int flag2 = 0;
 		for(int i = 0; i < longth-1; i++){
@@ -60,13 +22,13 @@ public:
 				break;
 			}
 		}
-		if(flag == 1){
-			for(int i = flag2; i < longth-1; i++){y
-				if( s[i] == '0' && s[i+1] == '1'){
-					result += 1;
-				}
-			}
-		}
+//		if(flag == 1){
+//			for(int i = flag2; i < longth-1; i++){y
+//				if( s[i] == '0' && s[i+1] == '1'){
+//					result += 1;
+//				}
+//			}
+//		}
 
 		return result;
 	}
