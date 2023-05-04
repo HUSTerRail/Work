a02c5a0a15cdf71873678ca35b44036b68659075
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-05-04 19:34:44 +0800

    2

--- src/work.cpp
+++ src/work.cpp
@@ -1,1 +1,27 @@
-222
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> s(n);
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> a[i];
+    }
+
+    int max_value = -1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            if (s[j].find(s[i]) != string::npos) {
+                max_value = max(max_value, a[i] + a[j]);
+            }
+        }
+    }
+    cout << max_value << endl;
+    return 0;
+}
