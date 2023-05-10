ed14d570b051e9e58172f02d0d4f85002df08f68
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-05-10 11:28:39 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,2 +1,22 @@
-123
-456
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+double str_to_double(string str)
+{
+    double result;
+    stringstream ss(str); // 使用stringstream将字符串转换为double型数据
+    ss >> result;
+    return result;
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    double num = str_to_double(str);
+    cout << num << endl;
+    return 0;
+}
