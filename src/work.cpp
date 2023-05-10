79ae4a2ab957c02f2de70bbaba61897ce957c658
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-05-10 10:31:04 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,49 +1,22 @@
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
-string add(string num1, string num2) {
-    double x, y;
-    stringstream ss1(num1), ss2(num2);
-    ss1 >> x;
-    ss2 >> y;
-    double sum = x + y;
-    stringstream ss;
-    ss << fixed << setprecision(2) << sum;
-    return ss.str();
+double str_to_double(string str)
+{
+    double result;
+    stringstream ss(str); // 使用stringstream将字符串转换为double型数据
+    ss >> result;
+    return result;
 }
 
-string sub(string num1, string num2) {
-    double x, y;
-    stringstream ss1(num1), ss2(num2);
-    ss1 >> x;
-    ss2 >> y;
-    double diff = x - y;
-    stringstream ss;
-    ss << fixed << setprecision(2) << diff;
-    return ss.str();
-}
-
-int main() {
-    string num1, num2, op;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    cout << "Enter an operator (+ or -): ";
-    cin >> op;
-
-    string result;
-    if (op == "+") {
-        result = add(num1, num2);
-    } else if (op == "-") {
-        result = sub(num1, num2);
-    } else {
-        cerr << "Invalid operator: " << op << endl;
-        return 1;
-    }
-
-    cout << "Result: " << result << endl;
+int main()
+{
+    string str;
+    cin >> str;
+    double num = str_to_double(str);
+    cout << num << endl;
     return 0;
 }
