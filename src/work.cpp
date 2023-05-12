950898cb3b9e420374d2d57634e70202fa73b9b6
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-05-12 22:46:51 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,6 +1,20 @@
-456
-sad
-asdsad
-asdasd
-sadasd
-asda
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    
+    getline(cin, str);
+    
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') { 
+            str[i] = str[i] - 32; 
+        }
+    }
+    
+    cout << str << endl;
+    return 0;
+}
+
