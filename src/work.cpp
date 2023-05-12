f7d9a2c760ef2cccc26170b2040707f0fce6de25
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-05-12 22:51:14 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
-    string str;
-    
-    getline(cin, str);
-    
-    for(int i=0; i<str.length(); i++) {
-        if(str[i] >= 'a' && str[i] <= 'z') { 
-            str[i] = str[i] - 32; 
+    int n;
+    string s, t;
+    cin >> n >> s;
+    int l = 0, r = n - 1; 
+    while (l <= r) { 
+        bool left = false;
+        for (int i = 0; l + i <= r; i++) { 
+            if (s[l + i] < s[r - i]) {
+                left = true;
+                break;
+            } else if (s[l + i] > s[r - i]) {
+                left = false;
+                break;
+            }
+        }
+        if (left) {
+            t += s[l];
+            l++;
+        } else {
+            t += s[r];
+            r--;
         }
     }
-    
-    cout << str << endl;
+    cout << t << endl;
     return 0;
 }
-
