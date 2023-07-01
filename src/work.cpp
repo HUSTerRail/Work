28b63d1e0328c0ccbfd2f689a670ffc45cdd4861
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-07-01 19:57:45 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,52 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+int getMinModification(const string& s) {
+    stack<char> st;
+    int modificationCount = 0;
 
-int convertToMinutes(const string& timeStr) {
-    int hours, minutes;
-    char delimiter;
-    stringstream ss(timeStr);
-    ss >> hours >> delimiter >> minutes;
-    return hours * 60 + minutes;
-}
-
-
-string convertToTime(int minutes) {
-    int hours = minutes / 60;
-    int mins = minutes % 60;
-    stringstream ss;
-    ss << hours / 10 << hours % 10 << ":" << mins / 10 << mins % 10;
-    return ss.str();
-}
-
-
-string getNextAlarmTime(const string& currentTime, const vector<string>& alarmTimes) {
-    int currentMinutes = convertToMinutes(currentTime);
-    int nextAlarmMinutes = 24 * 60; // 初始化为一个较大的值
-    for (const auto& alarmTime : alarmTimes) {
-        int alarmMinutes = convertToMinutes(alarmTime);
-        if (alarmMinutes > currentMinutes && alarmMinutes < nextAlarmMinutes) {
-            nextAlarmMinutes = alarmMinutes;
+    for (char c : s) {
+        if (c == '(' || c == '<' || c == '[' || c == '{') {
+            st.push(c);
+        } else {
+            if (st.empty()) {
+                modificationCount++;
+            } else {
+                char top = st.top();
+                st.pop();
+                if ((top == '(' && c != ')') ||
+                    (top == '<' && c != '>') ||
+                    (top == '[' && c != ']') ||
+                    (top == '{' && c != '}')) {
+                    modificationCount++;
+                }
+            }
         }
     }
-    return convertToTime(nextAlarmMinutes);
+
+    return modificationCount + st.size(); // 添加剩余左括号的修改次数
 }
 
 int main() {
-    string currentTime;
-    cin >> currentTime;
-    
-    int n;
-    cin >> n;
-    
-    vector<string> alarmTimes(n);
-    for (int i = 0; i < n; i++) {
-        cin >> alarmTimes[i];
-    }
-    
-    string nextAlarmTime = getNextAlarmTime(currentTime, alarmTimes);
-    cout << nextAlarmTime << endl;
-    
+    string s;
+    cin >> s;
+
+    int minModification = getMinModification(s);
+    cout << minModification << endl;
+
     return 0;
 }
-
