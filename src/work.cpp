ae119179d7043fb4ad11c27ca3aeed4810e1e6ab
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-07-01 17:33:39 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -3,29 +3,5 @@
 using namespace std;
 
 int main() {
-    int n;
-    string s, t;
-    cin >> n >> s;
-    int l = 0, r = n - 1; 
-    while (l <= r) { 
-        bool left = false;
-        for (int i = 0; l + i <= r; i++) { 
-            if (s[l + i] < s[r - i]) {
-                left = true;
-                break;
-            } else if (s[l + i] > s[r - i]) {
-                left = false;
-                break;
-            }
-        }
-        if (left) {
-            t += s[l];
-            l++;
-        } else {
-            t += s[r];
-            r--;
-        }
-    }
-    cout << t << endl;
     return 0;
 }
