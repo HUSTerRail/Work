d46c9a5d80ee0c2984dc361d9b5370ca27d1cbf5
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-07-01 20:18:48 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,31 +1,32 @@
+#include <iostream>
+#include <stack>
+#include <string>
 
-#include<bits/stdc++.h>
 using namespace std;
 
 int getMinModification(const string& s) {
-    stack<char> st;
     int modificationCount = 0;
+    stack<char> st;
 
     for (char c : s) {
         if (c == '(' || c == '<' || c == '[' || c == '{') {
-            st.push(c);
+            st.push(c); // 将左括号入栈
         } else {
-            if (st.empty()) {
-                modificationCount++;
-            } else {
-                char top = st.top();
-                st.pop();
-                if ((top == '(' && c != ')') ||
-                    (top == '<' && c != '>') ||
-                    (top == '[' && c != ']') ||
-                    (top == '{' && c != '}')) {
-                    modificationCount++;
+            if (!st.empty()) { // 如果栈不为空
+                char top = st.top(); // 获取栈顶元素
+                st.pop(); // 弹出栈顶元素
+                if ((top == '(' && c != ')') || (top == '<' && c != '>') ||
+                    (top == '[' && c != ']') || (top == '{' && c != '}')) {
+                    modificationCount++; // 如果当前字符与栈顶元素不匹配，则需要进行修改
                 }
+            } else {
+                // 如果没有对应的左括号，需要添加一个左括号
+                modificationCount++;
             }
         }
     }
 
-    return modificationCount + st.size()/2; // 添加剩余左括号的修改次数
+    return modificationCount + st.size()/2; // 添加剩余的左括号数量
 }
 
 int main() {
