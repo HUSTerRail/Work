8f0aaf0c414799a42583f31167eb4a6aae2785c2
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-08-12 12:01:29 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -8,7 +8,7 @@ bool find(int target, vector<bool>& used, vector<int>& match, vector<int>& w,map
     for(int i = 0; i <neighbors.size(); i++){ //遍历每个偶数与奇数比较
             if(w[neighbors[i]] == w[target] && used[i] ==0){
                  used[i] = true;
-                if(match[neighbors[i]] == 0 || find(match[neighbors[i]], used, match,w,graph)){ /
+                if(match[neighbors[i]] == 0 || find(match[neighbors[i]], used, match,w,graph)){ 
                     match[i] = target; //则配对该数
                     return true;
                 }
@@ -30,20 +30,20 @@ int main() {
     map<int, vector<int>>graph;
     vector<bool>visited(n+1,0);
 
-    while(cin >> u >>v){
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    // for(int i=0; i<2; i++){
-    //     cin >> u >>v;
+    // while(cin >> u >>v){
     //     graph[u].push_back(v);
     //     graph[v].push_back(u);
     // }
+    for(int i=0; i<2; i++){
+        cin >> u >>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
 
     int result = 0;
 
     for (int i = 1; i <= n; i++) {
-        if(find(i, visited, match, w, graph, i)){
+        if(find(i, visited, match, w, graph)){
             result++;
         }
         // find(int target, vector<bool>& used, vector<int>& match, vector<int>& w,map<int, vector<int>>&graph, int k){
