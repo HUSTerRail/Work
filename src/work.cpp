16c010d32fb0ac917fc175e983bf2357c9bf5931
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-08-19 00:32:39 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,1 +1,42 @@
-sasd
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string a;
+    string word;
+    vector<string>str;
+    cin >> a;
+    a +=':';
+    for(int i=0; i<a.length(); i++){
+
+        if(a[i] == ':'){
+            if(word.length() <=4 && word.length() > 0){
+                str.push_back(word);
+                if(str.size() > 8){
+                     cout << "No";
+                    return 0;
+                }
+                word = "";
+            }else{
+                 cout << "No";
+                return 0;
+            }
+            
+        }else{
+            if((a[i] >= '0' && a[i] <= '9')  ||  (a[i] >= 'A' && a[i] <= 'F') ||  (a[i] >= 'a' && a[i] <= 'f')){
+                 word += a[i];
+            }else{
+                cout << "No";
+                return 0;
+                
+            }
+           
+        }
+    }
+    
+    
+    cout << "Yes";
+    return 0;
+}
