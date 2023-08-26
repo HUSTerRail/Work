d603a4859aeb91f43437fdd15227874fae55c67b
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-08-26 10:33:27 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,42 +1,53 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
 
-    string a;
-    string word;
-    vector<string>str;
-    cin >> a;
-    a +=':';
-    for(int i=0; i<a.length(); i++){
 
-        if(a[i] == ':'){
-            if(word.length() <=4 && word.length() > 0){
-                str.push_back(word);
-                if(str.size() > 8){
-                     cout << "No";
-                    return 0;
-                }
-                word = "";
-            }else{
-                 cout << "No";
-                return 0;
-            }
-            
-        }else{
-            if((a[i] >= '0' && a[i] <= '9')  ||  (a[i] >= 'A' && a[i] <= 'F') ||  (a[i] >= 'a' && a[i] <= 'f')){
-                 word += a[i];
-            }else{
-                cout << "No";
-                return 0;
-                
-            }
-           
+int main() {
+    int n, m, tmp, q;
+    cin >> q;
+    
+    int flag = 0;
+    vector<int>a;
+    vector<int>b;
+    for(int w = 0; w<q; w++){
+        cin >> n >> m;
+        for(int i=0; i<n; i++){
+            cin >> tmp;
+            a.push_back(tmp);
         }
+
+        for(int i=0; i<n; i++){
+            cin >> tmp;
+            b.push_back(tmp);
+        }
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        for(int i=0; i<n; i++){
+            if(a[i] + b[n-1-i] < 0 || a[i] + b[n-1-i]>m){
+                cout<<i<<" "<<a[i]<<":"<< b[n-1-i]<<endl;
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 1){
+            cout<<"No"<<endl;
+            flag = 0;
+        }else{
+            cout << "Yes";
+        }
+        a = vector<int>();
+        b = vector<int>();
     }
     
-    
-    cout << "Yes";
+
+
+
+
+   
+
     return 0;
 }
