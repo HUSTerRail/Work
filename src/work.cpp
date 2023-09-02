36197896625edae4d04f0aedf0e7af7551fd4f9a
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-02 16:44:30 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void calcEditDistance(const string &str1, const string &str2, int &ins, int &del, int &sub) {
-    int m = str1.length();
-    int n = str2.length();
+void Distance(const string &string1, const string &string2, int &ins, int &del, int &sub) {
+    int m = string1.length();
+    int n = string2.length();
     
     vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
@@ -16,17 +12,16 @@ void calcEditDistance(const string &str1, const string &str2, int &ins, int &del
 
     for (int i = 1; i <= m; ++i) {
         for (int j = 1; j <= n; ++j) {
-            int cost = (str1[i-1] == str2[j-1]) ? 0 : 1;
+            int cost = (string1[i-1] == string2[j-1]) ? 0 : 1;
             dp[i][j] = min({dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + cost});
         }
     }
 
-    // Calculate number of insertions, deletions, and substitutions
     int i = m, j = n;
     ins = 0, del = 0, sub = 0;
     while (i > 0 || j > 0) {
-        if (i > 0 && j > 0 && dp[i][j] == dp[i-1][j-1] + (str1[i-1] != str2[j-1])) {
-            if (str1[i-1] != str2[j-1]) {
+        if (i > 0 && j > 0 && dp[i][j] == dp[i-1][j-1] + (string1[i-1] != string2[j-1])) {
+            if (string1[i-1] != string2[j-1]) {
                 ++sub;
             }
             --i;
@@ -42,16 +37,13 @@ void calcEditDistance(const string &str1, const string &str2, int &ins, int &del
 }
 
 int main() {
-    string str1, str2;
-    getline(cin, str1);
-    getline(cin, str2);
-
+    string string1, string2;
+    getline(cin, string1);
+    getline(cin, string2);
     int ins, del, sub;
-    calcEditDistance(str1, str2, ins, del, sub);
-
+    Distance(string1, string2, ins, del, sub);
     cout << ins << endl;
     cout << del << endl;
     cout << sub << endl;
-
     return 0;
 }
