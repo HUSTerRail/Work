6bae024da2827c7a50a4bcc54b4ec81cfa721c7b
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-02 19:15:16 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-    int n;
+    long int n;
     cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) {
+    vector<long int> a(n);
+    for(long int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    int res = 0;
-    for(int i = 1; i < n; i++) {
-        int cur = a[i];
-        int cnt = 0;
+    long int res = 0;
+    for(long int i = 1; i < n; i++) {
+        long int cur = a[i];
+        long int cnt = 0;
         while(cur > a[0]) {
             cur /= 2;
             cnt++;
