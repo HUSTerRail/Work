a704495390e5ce42c912b6d48811ca03671de0a7
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-02 19:50:09 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-
-    vector<int> a(n);
+    vector<int> arr(n);
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+        cin >> arr[i];
     }
 
-    int ans = 1e9;  // 初始化为一个大值
+    int min_ops = 1e9;  // 初始化最小操作次数为一个非常大的值
 
-    for (int i = 1; i < n; ++i) {
-        int cnt = 0;
-        int cur = a[i];
-        while (cur < a[0]) {
-            cur *= 2;
-            cnt++;
+    for (int target = arr[0]; target > 0; target /= 2) {  // 第一个元素有可能降低到的最低值
+        int ops = 0;
+        int max_val = target;
+
+        for (int i = 0; i < n; ++i) {
+            int val = arr[i], cnt = 0;
+
+            while (val > max_val) {  // 使元素尽可能降低到最低水平
+                val /= 2;
+                ++cnt;
+            }
+            while (val * 2 <= max_val) {  // 使元素尽可能增大到最高水平
+                val *= 2;
+                ++cnt;
+            }
+
+            ops += cnt;
+            max_val = max(max_val, val);  // 更新可能的最大值
         }
-        while (cur >= 2 * a[0]) {
-            cur /= 2;
-            cnt++;
-        }
-        ans = min(ans, cnt);
+
+        min_ops = min(min_ops, ops);
     }
 
-    cout << ans + 1 << endl;
+    cout << min_ops << endl;
 
     return 0;
 }
