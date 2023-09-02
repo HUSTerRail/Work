35cc93403bb7a508fc66bdb6d118025005fce329
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-02 19:36:51 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main() {
-    long int n;
+    int n;
     cin >> n;
-    vector<long int> a(n);
-    for(long int i = 0; i < n; i++) {
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
 
-    long int res = 0;
-    for(long int i = 1; i < n; i++) {
-        long int cur = a[i];
-        long int cnt = 0;
-        while(cur > a[0]) {
-            cur /= 2;
-            cnt++;
-        }
-        while(cur < a[0]) {
+    int ans = 1e9;  // 初始化为一个大值
+
+    for (int i = 1; i < n; ++i) {
+        int cnt = 0;
+        int cur = a[i];
+        while (cur < a[0]) {
             cur *= 2;
             cnt++;
         }
-        res = max(res, cnt);
+        while (cur >= 2 * a[0]) {
+            cur /= 2;
+            cnt++;
+        }
+        ans = min(ans, cnt);
     }
 
-    cout << res << endl;
+    cout << ans + 1 << endl;
+
     return 0;
 }
