f0355247998bc189f99838879d12749e7ac3c17d
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-06 19:46:51 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
-#include <algorithm>
-#include <climits>
 
 using namespace std;
 
-int minWoods(const vector<int>& woods, int length) {
-    vector<int> dp(length + 1, INT_MAX);  // 初始化为最大值
-    dp[0] = 0;  // 0长度不需要任何木材
-    for (int i = 0; i <= length; ++i) {
-        if (dp[i] != INT_MAX) {
-            for (int wood : woods) {
-                if (i + wood <= length) {
-                    dp[i + wood] = min(dp[i + wood], dp[i] + 1);
-                }
-            }
-        }
-    }
-    return dp[length] == INT_MAX ? -1 : dp[length];
-}
 
 int main() {
-    string line1, line2;
-    getline(cin, line1);
-    getline(cin, line2);
-    
-    istringstream iss(line1);
-    vector<int> woods;
-    int temp;
-    while (iss >> temp) {
-        woods.push_back(temp);
+    int k, m, n;
+    cin >> k >> m >> n;
+
+    if(k<(m+n)){
+        cout << "-1" << endl;
+        return 0;
+    }else{
+        vector<vector<int>>dp(m+1, vector<int>(n+1, 1));
+        for(int i=1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                // cout << dp[i-1][j] <<endl;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        cout << dp[m][n] << endl;
     }
-    
-    int length = stoi(line2);
-    
-    cout << minWoods(woods, length) << endl;
+ 
+
     return 0;
 }
