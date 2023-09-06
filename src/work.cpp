de14b34a6c073f8a74c329d2d96826ed269498b1
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-06 20:12:01 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+int maxGold(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<vector<int>>> visited(m, vector<vector<int>>(n, vector<int>(2, 0)));
+    queue<vector<int>> q;  // [x, y, used_skill, gold]
+    q.push({0, 0, 0, grid[0][0]});
+    int maxGold = 0;
 
-int main() {
-    int k, m, n;
-    cin >> k >> m >> n;
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
+    while (!q.empty()) {
+        auto curr = q.front();
+        q.pop();
+        int x = curr[0], y = curr[1], used_skill = curr[2], gold = curr[3];
 
-    if(k<(m+n)){
-        cout << "-1" << endl;
-        return 0;
-    }else{
-        vector<vector<int>>dp(m+1, vector<int>(n+1, 1));
-        for(int i=1; i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                // cout << dp[i-1][j] <<endl;
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        maxGold = max(maxGold, gold);
+        for (auto dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
+                if (grid[newX][newY] != -1 && !visited[newX][newY][used_skill]) {
+                    q.push({newX, newY, used_skill, gold + grid[newX][newY]});
+                    visited[newX][newY][used_skill] = 1;
+                } else if (grid[newX][newY] == -1 && !used_skill && !visited[newX][newY][1]) {
+                    q.push({newX, newY, 1, gold});
+                    visited[newX][newY][1] = 1;
+                }
             }
         }
-        cout << dp[m][n] << endl;
     }
- 
+    return maxGold;
+}
 
+int main() {
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int>> grid(m, vector<int>(n));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> grid[i][j];
+        }
+    }
+    cout << maxGold(grid) << endl;
     return 0;
 }
