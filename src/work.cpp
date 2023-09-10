a3f028b9c498083fb42af74bbb181d3a7ca67166
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-10 21:51:31 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,36 +1,81 @@
-#include <iostream>
-#include <cmath>
-#include <vector> 
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int getMaxHeight(int n, int a, int b) {
-    if (abs(a - b) > n - 1) {
-        return -1; 
+int n, k;
+vector<string> strs;
+vector<vector<int>> adjMatrix;
+vector<bool> visited;
+
+int longestCommonSubstring(const string& s1, const string& s2) {
+    int l1 = s1.size();
+    int l2 = s2.size();
+    vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
+    int maxLen = 0;
+
+    for (int i = 1; i <= l1; ++i) {
+        for (int j = 1; j <= l2; ++j) {
+            if (s1[i-1] == s2[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + 1;
+                maxLen = max(maxLen, dp[i][j]);
+            }
+        }
     }
-    int big = max(a, b);
-    int small = min(a, b);
-    int count = n - 2 - big + small;
-    int res = big;
-    res += count/2 + count%2;
-    res = max(big, res);
-    return res;
+    return maxLen;
+}
+
+void dfs(int node) {
+    visited[node] = true;
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i] && adjMatrix[node][i]) {
+            dfs(i);
+        }
+    }
+}
+
+int countGroups() {
+    fill(visited.begin(), visited.end(), false);
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            dfs(i);
+            count++;
+        }
+    }
+    return count;
 }
 
 int main() {
-    int T;
-    cin >> T;
-    vector<int> results;
-    
-    for (int i = 0; i < T; ++i) {
-        int n, a, b;
-        cin >> n >> a >> b;
-        results.push_back(getMaxHeight(n, a, b));
+    cin >> n >> k;
+    strs.resize(n);
+    adjMatrix.resize(n, vector<int>(n, 0));
+    visited.resize(n, false);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> strs[i];
     }
-    
-    for (int res : results) {
-        cout << res << endl;
+
+    vector<int> deleteOrder(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> deleteOrder[i];
+        deleteOrder[i]--;
     }
-    
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            if (longestCommonSubstring(strs[i], strs[j]) >= k) {
+                adjMatrix[i][j] = adjMatrix[j][i] = 1;
+            }
+        }
+    }
+
+    cout << countGroups() << endl;
+    for (int i = 0; i < n; ++i) {
+        strs[deleteOrder[i]] = "";
+        for (int j = 0; j < n; ++j) {
+            adjMatrix[deleteOrder[i]][j] = adjMatrix[j][deleteOrder[i]] = 0;
+        }
+        cout << countGroups() - i - 1 << endl;
+    }
+
     return 0;
 }
