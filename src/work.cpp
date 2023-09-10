9fb793110d57c5e161a992abc64bb4875ef45997
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-10 21:59:38 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,37 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k;
-vector<string> strs;
-vector<vector<int>> adjMatrix;
-vector<bool> visited;
-
-int longestCommonSubstring(const string& s1, const string& s2) {
-    int l1 = s1.size();
-    int l2 = s2.size();
-    vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
-    int maxLen = 0;
-
-    for (int i = 1; i <= l1; ++i) {
-        for (int j = 1; j <= l2; ++j) {
-            if (s1[i-1] == s2[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + 1;
-                maxLen = max(maxLen, dp[i][j]);
-            }
-        }
-    }
-    return maxLen;
-}
-
-void dfs(int node) {
-    visited[node] = true;
-    for (int i = 0; i < n; ++i) {
-        if (!visited[i] && adjMatrix[node][i]) {
-            dfs(i);
-        }
-    }
-}
-
 int countGroups() {
     fill(visited.begin(), visited.end(), false);
     int count = 0;
