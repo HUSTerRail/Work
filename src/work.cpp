0326f5181dd85eaee93b1f2d2186c3469c443f40
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-12 11:32:06 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -17,19 +17,37 @@ int main() {
 
     long int happiness = 0;
     long int count = 0;
+    int record = 0;
 
     for (long int i = 0; i < n; ++i) {
         if (beauty_values[i] > 0) {
+            record++;
             happiness += beauty_values[i];
             count++;
-        } else if (i + 2 < n && beauty_values[i] + beauty_values[i + 1] + beauty_values[i + 2] + k > 0) {
+            if(record == 3){
+                record = 0;
+                happiness += k;
+            }
+        } else if(record != 0){
+            if(record == 1){
+                if(i + 1 < n && beauty_values[i] + beauty_values[i + 1] + k > 0){
+                    happiness += beauty_values[i] + beauty_values[i + 1] + k;
+                    i += 1;
+                }
+            }else if(record == 2){
+                if(beauty_values[i] + k > 0){
+                    happiness += beauty_values[i] + k;
+                }
+            }
+            record = 0;
+        }else if (i + 2 < n && beauty_values[i] + beauty_values[i + 1] + beauty_values[i + 2] + k > 0) {
             happiness += beauty_values[i] + beauty_values[i + 1] + beauty_values[i + 2] + k;
             count += 3;
             i += 2;
         }
     }
 
-    happiness += (count / 3) * k;
+    // happiness += (count / 3) * k;
 
     cout << happiness << endl;
 
