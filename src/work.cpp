c1f199c3c2cfe2c7360afc5b53e86c15c26ff468
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-12 20:38:32 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -1,55 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cmath>
-#include <queue>
+
 using namespace std;
 
-const int maxn = 1e3 + 5;
-int a[maxn], b[maxn], n, k;
-
-bool check(int mid) {
-    vector<bool> vis(n, false);
-    int cnt = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (vis[i]) continue;
-
-        vis[i] = true;
-        queue<int> q;
-        q.push(i);
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (int v = 0; v < n; ++v) {
-                if (!vis[v] && abs(a[u]-a[v]) + abs(b[u]-b[v]) <= mid) {
-                    vis[v] = true;
-                    q.push(v);
-                }
-            }
-        }
-        ++cnt;
-    }
-
-    return cnt >= k;
+// 计算吸引度
+int calculateAttraction(int likes, int dislikes) {
+    return abs(likes - dislikes);
 }
 
 int main() {
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    for (int i = 0; i < n; ++i) cin >> b[i];
+    int n;
+    cin >> n;
 
-    int l = 0, r = 2e4, ans = -1; 
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (check(mid)) {
-            ans = mid;
-            l = mid + 1;
-        } else {
-            r = mid - 1;
-        }
+    vector<int> likes(n);
+    vector<int> dislikes(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> likes[i];
     }
 
-    cout << ans << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> dislikes[i];
+    }
+
+    int maxAttraction = 0;
+
+    // 枚举所有可能的组合
+    for (int i = 0; i < (1 << n); i++) {
+        int totalLikes = 0;
+        int totalDislikes = 0;
+
+        // 计算当前组合的点赞数和点踩数
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                totalLikes += likes[j];
+                totalDislikes += dislikes[j];
+            }
+        }
+
+        int attraction = calculateAttraction(totalLikes, totalDislikes);
+        maxAttraction = max(maxAttraction, attraction);
+    }
+
+    cout << maxAttraction << endl;
+
     return 0;
 }
