ee6f670130e2b12fc30302ed00c5746c2d1ae7f8
RAIL <87487131+HUSTerRail@users.noreply.github.com>
2023-09-12 11:03:06 +0800

    Update work.cpp

--- src/work.cpp
+++ src/work.cpp
@@ -10,12 +10,21 @@ int main() {
     vector<vector<int>> matrix(n, vector<int>(n, 1));
 
     for (int i = 0; i < n && k > 0; i++) {
-        for (int j = 0; j < n && k > 0; j++) {
-            if (matrix[i][j] == 1) {
+        for (int j = i; j < n && k > 0; j++) {
+            if (matrix[i][j] == 1 && k >= 2) {
                 matrix[i][j] = 0;
                 matrix[j][i] = 0;
                 if (i == j) k--;
-                else k -= 2;
+                else{
+                    k -= 2;
+                } 
+            }else{
+                if(i == j)
+                    matrix[i][j] = 0;
+                else{
+                    matrix[i + 1][i + 1] = 0;
+                }
+                k--;
             }
         }
     }
